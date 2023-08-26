ce59505ee80ba4bcc38471bbcf4c9c57aebde2be
michael <michaelmarav4@gmail.com>
2023-08-26 15:44:34 +0300

    update

--- src/racadim.py
+++ src/racadim.py
@@ -11,7 +11,7 @@ class Point:
 
 
 # Hyperparams
-real_time_plotting = True
+real_time_plotting = False
 robot_size = 1 # (m) Robot's diameter
 grid_resolution = 0.1 # (m) 
 workspace_size = (50,30) # (m) Size of the workspace/room where the robot needs to navigate
@@ -201,15 +201,14 @@ def ray_casting(x,y):
         
 
 
-        dis = 0#robot_size/grid_resolution - 5# Starting distance (radius) from ray casting 
+        dis = robot_size/grid_resolution # Starting distance (radius) from ray casting 
         prev_x = int(math.ceil(x))
         prev_y = int(math.ceil(y))
         while True and not path_found:
             dis += 1
-            if not stop_beam:
-                beam_x = int(math.ceil(x + dis * np.cos(angle_rad)))
-                beam_y = int(math.ceil(y - dis * np.sin(angle_rad)))
-
+            beam_x = int(math.ceil(x + dis * np.cos(angle_rad)))
+            beam_y = int(math.ceil(y - dis * np.sin(angle_rad)))
+            if not stop_beam and beam_x > 0 and beam_x < grid_size[0] and beam_y > 0 and beam_y < grid_size[1]:
                 # Found same ray
                 if grid[beam_x,beam_y] == 80:
                     plt.scatter([beam_x], [beam_y], color='purple', marker='o', s=50, label='Robot')
@@ -241,11 +240,7 @@ if __name__ == "__main__":
     init_grid()
 
     draw_grid()  
-    
-    # plot_occupancy_grid(grid)
 
-    # # print(grid[:,30])
-    # plt.show()
     it = 1
     max_it = 1
     while not path_found and it <= max_it:
@@ -253,7 +248,7 @@ if __name__ == "__main__":
             if robot_pos.shape[0] >= 1:
                 ray_casting(robot_pos[-1].x, robot_pos[-1].y)
                 robot_pos = np.delete(robot_pos, -1)
-                input("Press Enter to continue...")
+                #input("Press Enter to continue...")
             else:
                 break
 
@@ -279,3 +274,91 @@ if __name__ == "__main__":
         plt.pause(0.01)  # Pause to allow time for updates to be shown
     
     input("Press Enter to exit...")
+
+
+# Finds the intersection point
+
+'''
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+def find_intersection(x0, y0, x1, y1, x2, y2, r):
+    # Calculate direction vectors
+    segment_dir = (x2 - x1, y2 - y1)
+    semi_infinite_dir = (math.cos(r), math.sin(r))
+
+    # Solve for t and s
+    t_numerator = (x0 - x1) * semi_infinite_dir[1] - (y0 - y1) * semi_infinite_dir[0]
+    t_denominator = segment_dir[0] * semi_infinite_dir[1] - segment_dir[1] * semi_infinite_dir[0]
+    
+    if t_denominator == 0:
+        # Lines are parallel
+        return None
+
+    t = t_numerator / t_denominator
+
+    if t < 0 or t > 1:
+        # Intersection point is outside the line segment
+        return None
+
+    s = ((x1 - x0) + t * segment_dir[0]) / semi_infinite_dir[0]
+
+    if s < 0:
+        # Intersection point is behind the semi-infinite line
+        return None
+
+    # Calculate intersection point
+    intersection_x = x1 + t * segment_dir[0]
+    intersection_y = y1 + t * segment_dir[1]
+
+    if intersection_x >= 0 and intersection_x <= 10 and intersection_y >= 0 and intersection_y <= 10:
+        return (intersection_x, intersection_y)
+    else:
+        return None
+
+# Example inputs
+x0, y0 = 2,5
+x1, y1 = 1, 1
+x2, y2 = 9, 9
+r = math.radians(270)
+
+intersection = find_intersection(x0, y0, x1, y1, x2, y2, r)
+
+# Create a grid for visualization
+grid_size = 10
+grid = np.zeros((grid_size, grid_size))
+
+# Mark endpoints and intersection on the grid
+grid[int(y0), int(x0)] = 2  # Starting point
+grid[int(y1), int(x1)] = 1  # Endpoint 1
+grid[int(y2), int(x2)] = 1  # Endpoint 2
+
+# Plot the lines and points
+plt.figure(figsize=(8, 8))
+# plt.imshow(grid, origin='lower', cmap='cool', extent=[0, grid_size, 0, grid_size], alpha=0.3)
+
+# Plot the line segment
+plt.plot([x1, x2], [y1, y2], color='blue', label='Line Segment')
+
+# Plot the semi-infinite line
+line_length = 10
+line_end_x = x0 + line_length * math.cos(r)
+line_end_y = y0 + line_length * math.sin(r)
+plt.plot([x0, line_end_x], [y0, line_end_y], color='red', linestyle='dashed', label='Semi-Infinite Line')
+
+# Highlight the intersection point if it exists
+if intersection:
+    intersection_x, intersection_y = intersection
+    plt.scatter(intersection_x, intersection_y, color='green', label='Intersection Point')
+
+plt.title("Line and Intersection Visualization")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.legend()
+plt.grid()
+
+plt.show()
+
+
+'''
