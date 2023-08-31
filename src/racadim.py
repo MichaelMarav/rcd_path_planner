8a18c9758b87c75db54b9321246b6f63969fef50
michael <michaelmarav4@gmail.com>
2023-08-31 17:43:33 +0300

    fixed

--- src/racadim.py
+++ src/racadim.py
@@ -13,7 +13,7 @@ class Point:
 
 
 # Hyperparams
-real_time_plotting = False
+real_time_plotting = True
 robot_size = 1 # (m) Robot's diameter
 grid_resolution = 0.1 # (m) 
 workspace_size = (50,30) # (m) Size of the workspace/room where the robot needs to navigate
@@ -194,29 +194,38 @@ def find_angle_to_exclude(in_angle):
     print(res)
     return res
 
-def check_valid_cond(x,y,visited_points,closest_threshold):
+def check_closest_distance(x,y,visited_points,closest_threshold):
     for pos in visited_points:
         if math.sqrt((pos.x-x)**2 + (pos.y-y)**2) < closest_threshold: 
             return False
     return True 
 
-def find_intersection(grid,beam_x,beam_y):
-    
-    if grid[beam_x,beam_y] == 80:
-        return beam_x,beam_y
-    if grid[beam_x-1,beam_y] == 80:
-        return beam_x-1,beam_y
-    if grid[beam_x+1,beam_y] == 80:
-        return beam_x+1,beam_y
-    if  grid[beam_x,beam_y+1] == 80:
-        return beam_x,beam_y+1
-    if grid[beam_x,beam_y-1] == 80:
-        return beam_x,beam_y-1
 
 '''
-* Add line intersections
+Checks if the beam intersects with another one
+Condition--> Returns bool
 '''
-def ray_casting(x,y):
+def check_possible_intersection(grid,beam_x,beam_y,value):
+    if grid[beam_x,beam_y] == value:
+        return beam_x,beam_y
+    if grid[beam_x-1,beam_y] == value:
+        return beam_x-1,beam_y
+    if grid[beam_x+1,beam_y] == value:
+        return beam_x+1,beam_y
+    if  grid[beam_x,beam_y+1] == value:
+        return beam_x,beam_y+1
+    if grid[beam_x,beam_y-1] == value:
+        return beam_x,beam_y-1
+    return None, None
+
+
+'''
+Choose index to cast
+'''
+def choose_index(robot_pos):
+    pass
+
+def ray_casting_robot(x,y):
     global path_found,robot_pos,visited_robot,visited_target
 
     
@@ -243,22 +252,31 @@ def ray_casting(x,y):
                     stop_beam = True
 
                     # Don't crash with wall next to the point
-                    if check_valid_cond(beam_x,beam_y,visited_robot,10):
+                    if check_closest_distance(beam_x,beam_y,visited_robot,10):
                         valid_ray = True
                         robot_pos  = np.append(robot_pos,Point(prev_x,prev_y))
                         visited_robot  = np.append(visited_robot,Point(prev_x,prev_y))
 
                         # Plot with red the hit point
-                        plt.scatter([prev_x], [prev_y], color='red', marker='o', s=50, label='Robot')
+                        plt.scatter([prev_x], [prev_y], color='red', marker='o', s=20, label='Robot')
         
                 elif (grid[beam_x,beam_y] == 80 or grid[beam_x-1,beam_y] == 80 or grid[beam_x+1,beam_y] == 80 or grid[beam_x,beam_y+1] == 80 or grid[beam_x,beam_y-1] == 80):
                     stop_beam = True
-                    intersect_point_x,intersect_point_y = find_intersection(grid,beam_x,beam_y)
-                    if check_valid_cond(intersect_point_x,intersect_point_y,visited_robot,10):
+                    intersect_point_x,intersect_point_y = check_possible_intersection(grid,beam_x,beam_y,80)
+                    if intersect_point_x is not None and intersect_point_y is not None and check_closest_distance(intersect_point_x,intersect_point_y,visited_robot,10):
                         valid_ray = True
-                        plt.scatter(intersect_point_x,intersect_point_y,s = 80, color = 'purple')
+                        plt.scatter(intersect_point_x,intersect_point_y,s = 20, color = 'purple')
                         robot_pos  = np.append(robot_pos, Point(intersect_point_x,intersect_point_y))
                         visited_robot  = np.append(visited_robot,Point(intersect_point_x,intersect_point_y))
+                
+                elif (grid[beam_x,beam_y] == 40 or grid[beam_x-1,beam_y] == 40 or grid[beam_x+1,beam_y] == 40 or grid[beam_x,beam_y+1] == 40 or grid[beam_x,beam_y-1] == 40):
+                    path_found = True
+                    print("FOUND PATH by robot")
+                    valid_ray = True
+
+                    intersect_point_x,intersect_point_y = check_possible_intersection(grid,beam_x,beam_y,40)
+                    plt.scatter(intersect_point_x,intersect_point_y,s = 120, color = 'green')
+
                 else:
                     # For plotting with blue color where the beam has passed
                     indexes_to_change.append([beam_x, beam_y])
@@ -275,20 +293,96 @@ def ray_casting(x,y):
 
 
 
+def ray_casting_target(x,y):
+    global path_found,robot_pos,target_pos,visited_robot,visited_target
+
+    
+    for angle in range(0,360,int(360/num_beams)): # TODO: Add condition to stop if the arc length R dtheta is greater than the robot's size
+        indexes_to_change = []
+
+        angle_rad = np.radians(angle)
+
+        stop_beam = False # Stop casting flag
+        
+        valid_ray = False
+
+        dis = 3#robot_size/grid_resolution # Starting distance (radius) from ray casting 
+        prev_x = int(math.ceil(x))
+        prev_y = int(math.ceil(y))
+        while True and not path_found:
+            dis += 1
+            beam_x = int(math.ceil(x + dis * np.cos(angle_rad)))
+            beam_y = int(math.ceil(y - dis * np.sin(angle_rad)))
+           
+            if not stop_beam and beam_x > 0 and beam_x < grid_size[0] and beam_y > 0 and beam_y < grid_size[1]:
+                # Found same ray
+                if(grid[beam_x,beam_y] == 100):
+                    stop_beam = True
+
+                    # Don't crash with wall next to the point
+                    if check_closest_distance(beam_x,beam_y,visited_target,10):
+                        valid_ray = True
+                        target_pos     = np.append(target_pos,Point(prev_x,prev_y))
+                        visited_target = np.append(visited_target,Point(prev_x,prev_y))
+
+                        # Plot with red the hit point
+                        plt.scatter([prev_x], [prev_y], color='red', marker='o', s=20, label='Robot')
+        
+                elif (grid[beam_x,beam_y] == 40 or grid[beam_x-1,beam_y] == 40 or grid[beam_x+1,beam_y] == 40 or grid[beam_x,beam_y+1] == 40 or grid[beam_x,beam_y-1] == 40):
+                    stop_beam = True
+                    intersect_point_x,intersect_point_y = check_possible_intersection(grid,beam_x,beam_y,40)
+                    if intersect_point_x is not None and intersect_point_y is not None and check_closest_distance(intersect_point_x,intersect_point_y,visited_target,10):
+                        valid_ray = True
+                        plt.scatter(intersect_point_x,intersect_point_y,s = 20, color = 'purple')
+                        target_pos  = np.append(target_pos, Point(intersect_point_x,intersect_point_y))
+                        visited_target  = np.append(visited_target,Point(intersect_point_x,intersect_point_y))
+                elif (grid[beam_x,beam_y] == 80 or grid[beam_x-1,beam_y] == 80 or grid[beam_x+1,beam_y] == 80 or grid[beam_x,beam_y+1] == 80 or grid[beam_x,beam_y-1] == 80):
+                    valid_ray = True
+
+                    path_found = True
+                    intersect_point_x,intersect_point_y = check_possible_intersection(grid,beam_x,beam_y,80)
+                    print("FOUND PATH by target")
+                    plt.scatter(intersect_point_x,intersect_point_y,s = 120, color = 'green')
+
+                else:
+                    # For plotting with blue color where the beam has passed
+                    indexes_to_change.append([beam_x, beam_y])
+
+                prev_x = beam_x
+                prev_y = beam_y
+            
+            else: 
+                break
+
+        if valid_ray:
+            for row_idx, col_idx in indexes_to_change:
+                grid[row_idx,col_idx] = 40
+
+
 if __name__ == "__main__":
     # Add condition to create or to read occupancy grid
     init_grid()
 
     draw_grid()  
     cur_it = 1
-    while not path_found:
-        if robot_pos.shape[0] >= 1:
-            ray_casting(robot_pos[0].x,robot_pos[0].y)
-            robot_pos = np.delete(robot_pos,0)
-        else: 
-            print("Can not perform more ray casting") 
+    do_robot = True
+    while True:
+        if do_robot  and robot_pos.shape[0] >= 1:
+                ray_casting_robot(robot_pos[0].x,robot_pos[0].y)
+                # Add sophisticated condition for choosing casting index
+                robot_pos = np.delete(robot_pos,0)
+                do_robot = False
+
+        elif not do_robot and target_pos.shape[0] >= 1:
+                ray_casting_target(target_pos[0].x,target_pos[0].y)
+                target_pos = np.delete(target_pos,0)
+                do_robot = True
+        elif target_pos.shape[0] < 1 and robot_pos.shape[0] < 1 and not path_found: 
+            print("Could not find path") 
             print("Total Iterations = ", cur_it)
             break
+        else:
+            do_robot = not do_robot
 
 
         if real_time_plotting:
@@ -299,10 +393,15 @@ if __name__ == "__main__":
             plt.scatter(robot_beam_indices[0], robot_beam_indices[1], color='blue', s=2, label='RobotVirtual Beams')
 
             plt.pause(0.1)  # Pause to allow time for updates to be shown
-        
+            input("Press Enter to Continue...")
+
             # break # Remove this to check if it works
+
+        if path_found:
+            print("Found path")
+            print("Total Iterations = ", cur_it)
+            break
         cur_it += 1
-        # input("Press Enter to Continue...")
 
         
 
@@ -315,5 +414,16 @@ if __name__ == "__main__":
         plt.scatter(robot_beam_indices[0], robot_beam_indices[1], color='blue', s=2, label='RobotVirtual Beams')
 
         plt.pause(0.01)  # Pause to allow time for updates to be shown
-    
+
+    # Plot The path (grid values = 50 for path)
+
+    # fig2 = plt.figure()
+    # grid = np.where((grid != 0) & (grid != 100), 0, grid) # Removes casts
+
+    # im = plt.imshow(grid.T, cmap='binary', origin='upper', vmin=0, vmax=100)
+    # plt.scatter(robot_beam_indices[0], robot_beam_indices[1], color='blue', s=2, label='RobotVirtual Beams')
+    # plt.title('Figure 2')
+    # plt.xlabel('x')
+    # plt.ylabel('y')
+    plt.show()
     input("Press Enter to exit...")
