a5268285948c5be6a9774c969e54409d1ee9c92d
michael <michaelmarav4@gmail.com>
2023-09-29 16:34:38 +0300

    update gif

--- .gif/gif_maker.py
+++ .gif/gif_maker.py
@@ -2,4 +2,4 @@ from moviepy.editor import VideoFileClip
 
 
 videoClip = VideoFileClip("/home/michael/Videos/RCD.mp4")
-videoClip.write_gif("RCD.gif",fps = 60)
+videoClip.write_gif("RCD.gif",fps = 10)
